5978e1887fcd8f9e8cc6d529d6470c870e2e3260
Santhoshdevmj <104358799+Santhoshdevmj@users.noreply.github.com>
2025-06-26 06:46:25 +0000

    update app.ts title

--- angular-learning-app/src/app/app.ts
+++ angular-learning-app/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angular-learning-app';
+  protected title = 'Devsanthosh Angular Learning App';
 }
